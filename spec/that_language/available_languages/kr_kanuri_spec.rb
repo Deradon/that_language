a565d83ef08e6a0619b3eefbffea6425d6f2314a
Patrick Helm <me@patrick-helm.de>
2015-10-02 17:46:51 +0200

    Remove kr: kanuri as available language

--- spec/that_language/available_languages/kr_kanuri_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-require 'support/available_languages_helper'
-
-describe ThatLanguage, "kr (kanuri)" do
-  include AvailableLanguagesHelper
-  use_language_code "kr"
-
-  assert_that_language_with "<TODO>"
-end
